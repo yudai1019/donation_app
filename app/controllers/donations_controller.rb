6c65fd373787723e92b821f30b9960ff0afe8aab
yudai1019 <yudaydayo@gmail.com>
2022-07-20 15:19:55 +0900

    フォームオブジェクトパターン

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -5,20 +5,24 @@ class DonationsController < ApplicationController
   end
 
   def new
-
+    @donation_address = DonationAddress.new
   end
 
   def create
-    @donation = Donation.create(donation_params)
-    Address.create(address_params)
-    redirect_to root_path
+    @donation_address = DonationAddress.new(donation_params)
+    if @donation_address.valid?
+      @donation_address.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 end
 private
 
   def donation_params
-    params.permit(:price).merge(user_id: current_user.id)
+    params.require(:donation_address).permit(:postal_code, :prefecture, :city, :house_number, :building_name, :price).merge(user_id: current_user.id)
   end
 
   def address_params
--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,4 @@
 class Address < ApplicationRecord
   belongs_to :donation
-  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :prefecture, numericality: {other_than: 0, message: "can't be blank"}
+ 
 end
