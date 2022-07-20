b36782e31a466632788e3e87a46821d1aca3960a
yudai1019 <yudaydayo@gmail.com>
2022-07-20 14:29:58 +0900

    gem アクティブハッシュ

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -9,6 +9,18 @@ class DonationsController < ApplicationController
   end
 
   def create
+    @donation = Donation.create(donation_params)
+    Address.create(address_params)
+    redirect_to root_path
   end
 
 end
+private
+
+  def donation_params
+    params.permit(:price).merge(user_id: current_user.id)
+  end
+
+  def address_params
+    params.permit(:postal_code, :prefecture, :city, :house_number, :building_name).merge(donation_id: @donation.id)
+  end
--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,4 +1,7 @@
 class Donation < ApplicationRecord
   belongs_to :user
   has_one :address
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: "is invalid"}
 end
+
+
